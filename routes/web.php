f0a37eedf15a476dfb5d9cc1d9e6db0a6a3fc5d9
Lucas-Schissel <lucas.schissel@gmail.com>
2020-05-17 14:46:54 -0300

    1º Envio / Alteraçoes de Layout

--- routes/web.php
+++ routes/web.php
@@ -7,27 +7,67 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/logout', 'AppController@logout')
-	->name('logout');
-
 //-------------------------------------------------------
 //Apicaçao Routes----------------------------------------
 Route::middleware(['auth'])->group(function(){
 
     //Demais Rotas devem ficar aqui dentro---------------
 
+    Route::get('/dashboard', 'AppController@dashboard')
+	->name('dashboard');
+
+    Route::get('/logout', 'AppController@logout')
+	->name('logout');
+
     Route::get('/home', 'HomeController@home')
     ->name('home');
 
+    //Clientes................................................................................
 
+	Route::get('/cliente/cadastro', 'ClienteController@telaCadastro')
+	->name('cliente_cadastro');
 
+	Route::get('/cliente/alterar/{id}', 'ClienteController@telaAlteracao')
+	->name('cliente_update');
 
+	Route::post('/cliente/adicionar', 'ClienteController@adicionar')
+	->name('cliente_add');
 
+	Route::post('/cliente/alterar/{id}', 'ClienteController@alterar')
+	->name('cliente_alterar');
 
+	Route::get('/cliente/excluir/{id}', 'ClienteController@excluir')
+	->name('cliente_delete');
 
+	Route::get('/cliente/listar', 'ClienteController@listar')
+	->name('cliente_listar');
 
+	//Produtos................................................................................
+
+	Route::get('/processo/cadastro', 'processoController@telaCadastro')
+	->name('processo_cadastro');
+
+	Route::get('/processo/alterar/{id}', 'processoController@telaAlteracao')
+	->name('processo_update');
+
+	Route::post('/processo/adicionar', 'processoController@adicionar')
+	->name('processo_add');
+
+	Route::post('/processo/alterar/{id}', 'processoController@alterar')
+	->name('processo_alterar');
+
+	Route::get('/processo/excluir/{id}', 'processoController@excluir')
+	->name('processo_delete');
+
+	//Clientes..................................................................................
+	Route::get('/cliente/listar', 'ClienteController@listar')
+	->name('cliente_listar');
+
+	//Produtos................................................................................
+	Route::get('/processos/listar', 'ProcessoController@listar')
+	->name('processos_listar');
 
 });
-//-------------------------------------------------------
+
 
 
